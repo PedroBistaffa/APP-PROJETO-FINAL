900f6a523f855ca780c7bd1b8f17f313c72f7741
Pedro Henrique Bistaffa Pizoletto <Pedro Henrique Bistaffa Pizoletto>
2025-10-23 21:29:33 -0300

    atualização-controllers

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from core.aluno.aluno_controller import aluno_controller
 from core.usuario.usuario_controller import usuario_controller
 from core.professor.professor_controller import professor_controller
+from core.materia.materia_controller import materia_controller
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(aluno_controller)
 app.register_blueprint(usuario_controller)
 app.register_blueprint(professor_controller)
+app.register_blueprint(materia_controller)
 
 if __name__ == "__main__":
     app.run(debug=True)
